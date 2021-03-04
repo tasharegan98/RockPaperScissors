dab2ea347c703ea404e8e7cfc2fd310d02d8183f
Natasha Regan <natasharegan2@gmail.com>
2021-03-04 08:59:13 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,61 +1,81 @@
-var button = document.querySelector("#button")
-var pictures = [
-  {
-    text: "rock",
-    img: "rock.jpg"
-  },
-  {
-    text: "paper",
-    img: "paper.jpg"
-  },
-  {
-    text: "scrissors",
-    img: "scissors.jpg"
-  }
-];
+var displayResult = document.getElementById('#answer')
+var scoreP1 = document.querySelector('#scorep1')
+var scoreP2 = document.querySelector('#scorep2')
 
-button.addEventListener('click',(e) => {
-  e.preventDefault();
- randomChoice();
- assignImage();
- mapPictures();
-})
+var tallyScores = [0,0]
 
-function randomChoice() {  
-var image1 = pictures[Math.floor(Math.random()* pictures.length)];
+var pictureSelection = new Array("images/rock.jpg","images/paper.jpg", "images/scissors.jpg")
 
-var image2 = pictures[Math.floor(Math.random()* pictures.length)];
+//rock click
+var rockBtn = document.querySelector("#rock")
+  var rockImage = pictureSelection[0]
+  var userPic = document.getElementById('userpic')
+  rockBtn.addEventListener('click', (e) => {
+  userPic.style.display='block';
+  userPic.src = pictureSelection[0];
+  randomImage();
+} )
 
-  return {
-    image1, image2
+//paper click 
+var paperBtn = document.querySelector("#paper")
+  var paperImage = pictureSelection[1]
+  var userPic = document.getElementById('userpic')
+  paperBtn.addEventListener('click', (e) => {
+  userPic.style.display='block';
+  userPic.src = pictureSelection[1]
+  randomImage();
+} )
+
+//scissors click
+var scissorsBtn = document.querySelector("#scissors")
+  var scissorsImage = pictureSelection[2]
+  var scissorsPic = document.getElementById('userpic')
+  scissorsBtn.addEventListener('click', (e) => {
+  userPic.style.display='block';
+  userPic.src = pictureSelection[2]
+  randomImage();
+} )
+
+//function for img2 
+function randomImage() { 
+  //randomly select image from array
+  var image2 = Math.floor(Math.random()* pictureSelection.length);
+  //output image to DOM
+  var robotArmPic = document.querySelector("#myPicture");robotArmPic.src = pictureSelection[image2];
+  robotArmPic.style.display='block'
+  document.querySelector('robotResult')
+  var robotIndex = JSON.stringify(image2);
+  console.log(robotIndex)
+  return{
+    robotIndex
     }
 }
 
-function assignImage () {
-let {image1, image2} = randomChoice();
-document.querySelector("#img1").innerHTML =  
-'<img src="' + image1.img + '" width="250" height="250">' + '<p>' + image1.text + '</p>'  ;
 
-document.querySelector("#img2").innerHTML =  
-  '<img src="' + image2.img + '" width="250" height="250">' + '<p>' + image2.text + '</p>'  ;
+let robotChoice = randomImage();
+//0 = Rock 1= Paper 2=Scissors
+
+
+
+/*
+//compare results from players
+let result = checkWinner(userResult, robotResult)
+
+if (result === 'myArm'){
+  result += 'wins';
+  tallyScores [0]++
 }
 
-function mapPictures() {
-
-  const keyArray = Object.keys(pictures)
-
-  for (const key of keyArray){
-    console.log(key)
-  }
- 
-//0 = rock, 1 = paper, 2=scissors 
-//console.log(pictures[val]);
-  
+if (result === 'robotArm'){
+  result += 'wins';
+  tallyScores [0]++
 }
 
+if(result === 'Draw'){
+  results += 'Tie, play again'
+}
 
+//output scores tn DOM
+scoreP1.innerHTML = `Player1: [`+tallyScores[0]+`] Player2 : [`+ tallyScores [1] +`]` ;
 
-
-
-
-
+*/
